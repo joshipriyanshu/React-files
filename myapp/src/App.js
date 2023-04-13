6b4ad7a6a9b402396fb181a8cb8869249af9a2c9
Priyanshu <Priyanshujoshi2020@gmail.com>
2023-04-13 12:04:59 +0530

    created a new folder

---
+++ myapp/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./style.css"
+import Button from "./Button"
+
+
+
+const App =  () => {
+    return( 
+     <div>
+    <h1 style={{textAlign: "center"}} >  Hello world </h1>
+    <Button title={"Playstore"}> </Button>
+    <Button title={"App Store"}> </Button>
+    
+    </div>
+    
+  )}
+
+export default  App
