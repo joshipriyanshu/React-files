28e0cc0af19743d74c6b227eb88894ddbb58084b
Priyanshu <Priyanshujoshi2020@gmail.com>
2023-07-28 00:09:22 +0530

    Created Random user api UI

--- newrandomapp/src/App.js
+++ newrandomapp/src/App.js
@@ -1,24 +1,51 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout'
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
+import React, {useState, useEffect} from 'react'
+
+import {Container, Row, Col} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import './index.css'
+
+
+import Axios from "axios"
+import MyCard from './Card'
+
 
 const App = () => {
+
+  const [details, setdetails] =useState({})
+ const fetchDetails = async () => {
+
+  const {data} = await Axios.get("https://randomuser.me/api/")
+  console.log("response:",  data)
+
+  const details = data.results[0] 
+
+ setdetails(details) 
+
+
+ }
+
+ useEffect(() => {
+  fetchDetails()
+  console.log("iam use state")
+
+ }, [])
+
+
   return (
-   <>
-   <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />} >
-          <Route path='home' element={<Home />} />
-          <Route path='about' element={<About />} />
-          <Route path='contact' element={<Contact />} />
-        </Route>
-      </Routes>
-   </BrowserRouter>
-   </>
-  )
+  <Container fluid className='p-4 bg-primary App'>
+    <Row>
+    <Col md={4} className=" offset-md-4 mt-4">
+      <MyCard details = {details}/>
+    </Col>
+
+
+    </Row>
+    </Container>
+);
+   
+
+
+  
 }
 
 export default App
